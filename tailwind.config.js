bb135fd5f77d42f343444237b9c5f367474bb08a
shraddha319 <shraddha.jain3219@gmail.com>
2021-07-31 19:52:20 +0530

    style: config font family, custom colors, reset sm screen size

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,27 @@ module.exports = {
   purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        customGray: "#232323",
+        primary: "#E63E2D",
+        gold: "#CDB750",
+        secondary: "#CDB750",
+      },
+
+      screens: {
+        sm: "360px",
+      },
+    },
+    fontFamily: {
+      hp: "parry-hotter",
+      filsonThin: "filson-thin",
+      filsonRegular: "filson-regular",
+      filsonMedium: "filson-medium",
+      filsonBold: "filson-bold",
+    },
   },
   variants: {
     extend: {},
   },
-  plugins: [],
 };
