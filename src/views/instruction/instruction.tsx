46d308ebb0069f5e1d6195d126ae79cf1d781e41
shraddha319 <shraddha.jain3219@gmail.com>
2021-07-31 19:55:26 +0530

    feat: quiz instructions page for small screen

--- src/views/instruction/instruction.tsx
+++ src/views/instruction/instruction.tsx
@@ -1,4 +1,3 @@
-import "./instruction.scss";
 import { useQuiz } from "../../context/quiz";
 
 import { Link } from "react-router-dom";
@@ -9,12 +8,50 @@ export default function Instruction() {
   } = useQuiz();
 
   return (
-    <div className="instruction layout--default">
-      <h1 className="instruction__title">{quizData.quizName}</h1>
-      <h2>Welcome {session.username}!</h2>
-      <h3>Total Questions: {session.categorySelected.questions.length}</h3>
-      <h3>Time: 10 min</h3>
-      <Link to="/quiz">start</Link>
+    <div className="space-y-4 flex flex-col items-center px-4 py-2">
+      <h1 className="text-lg text-center uppercase tracking-widest text-primary">
+        {session.categorySelected.category}
+      </h1>
+      <p className="text-xs text-justify text-customGray">
+        {session.categorySelected.description}
+      </p>
+      <h1 className="text-lg text-center uppercase tracking-widest text-primary">
+        Instructions
+      </h1>
+      <ul className="list-outside text-xs list-disc m-6 space-y-2">
+        <li>
+          There are a total of{" "}
+          <span className="text-primary">
+            {session.categorySelected.questions.length} questions.
+          </span>
+        </li>
+        <li>
+          Each question has a time limit of{" "}
+          <span className="text-primary">15 seconds.</span>
+        </li>
+        <li>
+          Click on the <span className="text-primary">Next</span> button to go
+          to the next question, <span className="text-primary">Quit</span> to
+          end the game.
+        </li>
+        <li>
+          Each correct answer will be awarded 1 point. 0 for incorrect answers.
+        </li>
+      </ul>
+      <div>
+        <Link
+          className="inline-block text-sm text-white bg-primary py-2 px-4 mx-1 rounded "
+          to="/quiz"
+        >
+          Start
+        </Link>
+        <Link
+          className="inline-block text-sm text-customGray border-2 border-customGray mx-1 py-2 px-4 rounded "
+          to="/category"
+        >
+          Go Back
+        </Link>
+      </div>
     </div>
   );
 }
