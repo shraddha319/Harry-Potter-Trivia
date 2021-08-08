918bc3d2f0c0e936cd3a0288bdcf58602380f93a
shraddha319 <shraddha.jain3219@gmail.com>
2021-08-08 20:50:29 +0530

    fix: only 'required' validation needed on login

--- src/lib/validation/login.js
+++ src/lib/validation/login.js
@@ -4,17 +4,8 @@ const { EMAIL_INVALID, PASSWORD_INVALID } = formError;
 export const loginValidationRules = {
   email: {
     required: true,
-    format: {
-      regexp: /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
-      message: EMAIL_INVALID,
-    },
   },
   password: {
     required: true,
-    minLen: 8,
-    format: {
-      regexp: /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/,
-      message: PASSWORD_INVALID,
-    },
   },
 };
