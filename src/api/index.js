841ded8e397d66dd7f210000f9be9d4b5ae501a0
shraddha319 <shraddha.jain3219@gmail.com>
2021-09-12 04:01:25 +0530

    fix: api update; rename user to users route

--- src/api/index.js
+++ src/api/index.js
@@ -1,12 +1,12 @@
-import { getQuiz } from "./quiz.api";
+import { getQuiz } from './quiz.api';
 import {
   registerUser,
   verifyIfAvailable,
   loginUser,
   getUser,
-} from "./user.api";
-import { getLeaderboard, postUserScore } from "./history.api";
-import API from "./config.api";
+} from './user.api';
+import { getLeaderboard, postUserScore } from './score.api';
+import API from './config.api';
 
 export {
   API,
--- src/api/user.api.js
+++ src/api/user.api.js
@@ -1,19 +1,19 @@
-import API from "./config.api";
+import API from './config.api';
 
 export async function registerUser(user) {
-  return API.post("/user", { user });
+  return API.post('/users', { user });
 }
 
 export async function verifyIfAvailable({ key, value }) {
-  return API.head("/user", {
+  return API.head('/users', {
     headers: { key, value },
   });
 }
 
 export async function loginUser(credentials) {
-  return API.post("/auth/login", { ...credentials });
+  return API.post('/auth/login', { ...credentials });
 }
 
 export async function getUser(userId) {
-  return API.get(`/user/${userId}`);
+  return API.get(`/users/${userId}`);
 }
