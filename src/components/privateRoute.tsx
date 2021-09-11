7d4be85628808418a48f5e73299a7b89849b6893
shraddha319 <shraddha.jain3219@gmail.com>
2021-09-12 04:03:48 +0530

    fix: add replace prop while redirect

--- src/components/privateRoute.tsx
+++ src/components/privateRoute.tsx
@@ -1,5 +1,5 @@
-import { Navigate, Route } from "react-router-dom";
-import { useAuth } from "../context";
+import { Navigate, Route } from 'react-router-dom';
+import { useAuth } from '../context';
 
 export default function PrivateRoute({ path, ...props }) {
   const {
@@ -9,6 +9,6 @@ export default function PrivateRoute({ path, ...props }) {
   return authToken ? (
     <Route {...props} path={path} />
   ) : (
-    <Navigate to="/login" state={{ from: path }} replace />
+    <Navigate replace to="/login" state={{ from: path }} replace />
   );
 }
