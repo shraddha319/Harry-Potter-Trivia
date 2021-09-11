c288a7497b96ea9158920f31c0e89443a09d689b
shraddha319 <shraddha.jain3219@gmail.com>
2021-09-12 04:02:23 +0530

    refactor: selected houseIcon component

---
+++ src/components/houseIcon.tsx
@@ -0,0 +1,33 @@
+import { ReactComponent as Gryffindor } from "../images/Gryffindor.svg";
+import { ReactComponent as Slytherin } from "../images/Slytherin.svg";
+import { ReactComponent as Hufflepuff } from "../images/Hufflepuff.svg";
+import { ReactComponent as Ravenclaw } from "../images/Ravenclaw.svg";
+import { useQuiz } from "../context";
+
+export default function HouseIcon() {
+  const {
+    quiz: { theme },
+  } = useQuiz();
+
+  switch (theme) {
+    case "gryffindor":
+      return (
+        <Gryffindor className="w-32 h-32 hover:bg-gray-200" fill="#E63E2D" />
+      );
+
+    case "slytherin":
+      return (
+        <Slytherin className="w-32 h-32 hover:bg-gray-200" fill="#1C5B4C" />
+      );
+
+    case "hufflepuff":
+      return (
+        <Hufflepuff className="w-32 h-32 hover:bg-gray-200" fill="#CCB750" />
+      );
+
+    case "ravenclaw":
+      return (
+        <Ravenclaw className="w-32 h-32 hover:bg-gray-200" fill="#021A26" />
+      );
+  }
+}
