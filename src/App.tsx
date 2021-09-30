5ccf14670f212233cee7cbd292e046b66362414c
shraddha319 <shraddha.jain3219@gmail.com>
2021-09-30 16:55:11 +0530

    feat: add scroll to top

--- src/App.tsx
+++ src/App.tsx
@@ -8,41 +8,48 @@ import {
   Quiz,
   Score,
   Theme,
-  LeaderBoard,
   UserHistory,
+  LeaderBoard,
   Login,
   Signup,
   Profile,
 } from './views';
-import { Header, PrivateRoute } from './components';
-import { useQuiz } from './context';
+import { Header, PrivateRoute, ScrollToTop, Loader } from './components';
+import { useQuiz, useAuth } from './contexts';
 
 export default function App() {
   const {
     quiz: { theme },
   } = useQuiz();
+  const { auth } = useAuth();
 
   return (
     <div className={`App font-filsonMedium w-screen h-screen ${theme}`}>
       <Header />
-      <main className="h-full sm:mt-10">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/category" element={<Category />} />
-          <Route
-            path="/category/:categoryId/instruction"
-            element={<Instruction />}
-          />
-          <Route path="/category/:categoryId/play" element={<Quiz />} />
-          <Route path="/category/:categoryId/score" element={<Score />} />
-          <Route path="/theme" element={<Theme />} />
-          <PrivateRoute path="/scores" element={<UserHistory />} />
-          <PrivateRoute path="/leaderboard" element={<LeaderBoard />} />
-          <PrivateRoute path="/profile" element={<Profile />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/signup" element={<Signup />} />
-        </Routes>
-      </main>
+      <ScrollToTop>
+        {auth.status === 'loading' ? (
+          <Loader />
+        ) : (
+          <main className="h-full sm:mt-10">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/category" element={<Category />} />
+              <Route
+                path="/category/:categoryId/instruction"
+                element={<Instruction />}
+              />
+              <Route path="/category/:categoryId/play" element={<Quiz />} />
+              <Route path="/category/:categoryId/score" element={<Score />} />
+              <Route path="/theme" element={<Theme />} />
+              <PrivateRoute path="/scores" element={<UserHistory />} />
+              <PrivateRoute path="/leaderboard" element={<LeaderBoard />} />
+              <PrivateRoute path="/profile" element={<Profile />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/signup" element={<Signup />} />
+            </Routes>
+          </main>
+        )}
+      </ScrollToTop>
     </div>
   );
 }
---
+++ src/components/scrollToTop.tsx
@@ -0,0 +1,16 @@
+import { useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
+
+export default function ScrollToTop({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const location = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [location]);
+
+  return <>{children}</>;
+}
