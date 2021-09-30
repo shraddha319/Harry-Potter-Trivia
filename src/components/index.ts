0a8ba9597ce20f2550677c0c247203d393c8ca59
shraddha319 <shraddha.jain3219@gmail.com>
2021-09-30 16:54:45 +0530

    fix: update file expension to tsx

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,7 +1,8 @@
-import Header from "./header";
-import PrivateRoute from "./privateRoute";
-import Loader from "./loader";
-import Alert from "./alert";
-import HouseIcon from "./houseIcon";
+import Header from './header';
+import PrivateRoute from './privateRoute';
+import Loader from './loader';
+import Alert from './alert';
+import HouseIcon from './houseIcon';
+import ScrollToTop from './scrollToTop';
 
-export { Header, PrivateRoute, Loader, Alert, HouseIcon };
+export { Header, PrivateRoute, Loader, Alert, HouseIcon, ScrollToTop };
